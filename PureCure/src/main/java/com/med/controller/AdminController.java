03c21db5bb1cb6ad6cb287670fde1e9124aeb3ba
sanjay patel <sanjayhit96@gmail.com>
2023-12-04 12:13:01 +0530

    Creating PostMapping for Admin

--- PureCure/src/main/java/com/med/controller/AdminController.java
+++ PureCure/src/main/java/com/med/controller/AdminController.java
@@ -1,5 +1,29 @@
 package com.med.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.med.exception.AdminAlreadyExistsException;
+import com.med.model.Admin;
+import com.med.serviceinetrface.AdminService;
+
+@RestController
 public class AdminController {
+	
+	@Autowired
+	private AdminService adminService;
+	
+	@PostMapping
+	public ResponseEntity<Admin> addNewAdmin(@RequestBody Admin admin) throws AdminAlreadyExistsException{
+		
+	 	Admin ad = adminService.addNewAdmin(admin);
+	 	
+	 	return new ResponseEntity<Admin>(ad, HttpStatus.CREATED);
+	}
 
 }
--- PureCure/src/main/java/com/med/serviceimplementation/AdminServiceImpl.java
+++ PureCure/src/main/java/com/med/serviceimplementation/AdminServiceImpl.java
@@ -1,10 +1,13 @@
 package com.med.serviceimplementation;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.med.exception.AdminAlreadyExistsException;
+import com.med.exception.AdminException;
 import com.med.model.Admin;
 import com.med.repository.AdminRepository;
 import com.med.serviceinetrface.AdminService;
@@ -16,9 +19,15 @@ public class AdminServiceImpl implements AdminService{
 	private AdminRepository adminRepo;
 
 	@Override
-	public Admin addNewAdmin(Admin admin) {
+	public Admin addNewAdmin(Admin admin) throws AdminAlreadyExistsException {
 		// TODO Auto-generated method stub
-		return null;
+		Optional<Admin> op =  adminRepo.findByEmail(admin.getAdminEmail());
+		if (!op.isPresent()) {
+	        adminRepo.save(admin);
+	    } else {
+	        throw new AdminAlreadyExistsException("Admin with email " + admin.getAdminEmail() + " already exists.");
+	    }
+		return admin;
 	}
 
 	@Override
