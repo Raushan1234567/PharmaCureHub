94fa46d7456b191973622e7c3e005d5b6e1daf79
Raushan1234567 <raushankumar241099@gmail.com>
2023-12-09 21:16:01 +0530

    User able to create their account using front end

--- PureCure/src/main/java/com/med/serviceimplementation/SignupDtoImlementation.java
+++ PureCure/src/main/java/com/med/serviceimplementation/SignupDtoImlementation.java
@@ -1,8 +1,11 @@
 package com.med.serviceimplementation;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.med.exception.CustomerException;
 import com.med.model.Customer;
 import com.med.model.SignupDto;
 import com.med.repository.CustomerRepository;
@@ -17,12 +20,18 @@ public class SignupDtoImlementation implements SignupDtoInteface{
         @Override
 	    public void signUpUser(SignupDto signupDto) {
 	     
+        	Optional<Customer> customer=customerRepository.findBycustomerEmail(signupDto.getCustomerEmail());
+        	if(customer.isEmpty()) {
 	        Customer newCustomer = new Customer();
-	        newCustomer.setCustomerfirstName(signupDto.getCustomerFirstName());
+	        newCustomer.setCustomerfirstName(signupDto.getCustomerfirstName());
 	        newCustomer.setCustomerEmail(signupDto.getCustomerEmail());
 	        newCustomer.setCustomerPassword(signupDto.getCustomerPassword());
 
 	        customerRepository.save(newCustomer);
+        	}
+        	else {
+        		throw new CustomerException("Alraedy have an acount");
+        	}
 
 	    }
 }
